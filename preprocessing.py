4e728c279d1835fb986c5c2bd70dd259f6ff24bc
sorXCode <sorXCode@gmail.com>
2019-06-19 16:06:45 +0100

    fixed cleaning algos

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,43 @@
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 import re
 from textblob import TextBlob
+import numpy as np
 
 
 def clean_tweet(tweet):
+    # from nltk.tokenize import word_tokenize
+    # cleaned = word_tokenize(tweet)
+    # cleaned = [word for word in cleaned if word.isalpha() or word.isalnum()]
+    # print(tweet)
+    tweet = ascii(tweet)
     cleaned = ' '.join(re.sub(
-              r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)",
+              r"(\w+:\/\/\S+)|(RT)|(\\u\S+)|(#)",
               " ", tweet).split())
-    text = re.search(r'text(.+)source',
-                     cleaned).group(1)
-    print(text)
-    return text
-
+    # print(cleaned, end="\n\n")
+    cleaned = ' '.join(re.sub(
+              r"\\n+",
+              '. ', cleaned).split())
+#     text = re.search(r'text(.+)source',
+#                      cleaned).group(1)
+    print(cleaned[1:-2])
 
 # tweet = "\"created_at\":\"Thu Jun 13 11:14:00 +0000 2019\",\"id\":1139128782344916992,\"id_str\":\"1139128782344916992\",\"text\":\"You'll surely fall in love with Ashley's charm and beauty just like how it captured Tan's heart! \\ud83d\\ude0d\\ud83d\\ude0d\\ud83d\\ude0d Check out why\\u2026 https:\\/\\/t.co\\/kUsMJqOkqN\",\"source\":\"\\u003ca href=\\\"https:\\/\\/about.twitter.com\\/products\\/tweetdeck\\\" rel=\\\"nofollow\\\"\\u003eTweetDeck\\u003c\\/a\\u003e\",\"truncated\":true,\"in_reply_to_status_id\":null,\"in_reply_to_status_id_str\":null,\"in_reply_to_user_id\":null,\"in_reply_to_user_id_str\":null,\"in_reply_to_screen_name\":null,\"user\":{\"id\":31313277,\"id_str\":\"31313277\",\"name\":\"ABS-CBN Corporation\",\"screen_name\":\"ABSCBN\",\"location\":\"Manila, Philippines\",\"url\":\"http:\\/\\/www.entertainment.abs-cbn.com\",\"description\":\"The official Twitter account of ABS-CBN, the Philippines' largest network. http:\\/\\/facebook.com\\/ABSCBNnetwork\\/ https:\\/\\/www.instagram.com\\/abscbn\\/\",\"translator_type\":\"none\",\"protected\":false,\"verified\":true,\"followers_count\":996662,\"friends_count\":743,"
 # sentiment = TextBlob(clean_tweet(tweet))
 # st = sentiment.sentiment.polarity
 # print(st)
 
-text = "it's safe to say that @elonmusk and @TeslaMotors' ambition to \
-        accelerate the advent of sustainable transport is successful :-)"
+# text = "it's safe to say that @elonmusk and @TeslaMotors' ambition to \
+#         accelerate the advent of sustainable transport is a #success :-)"
 
-print(TextBlob(text).sentiment.polarity)
+# print(TextBlob(text).sentiment.polarity)
+
+
+d_tweet = "Juventus vs Porto\nBarcelona vs Ajax\nLiverpool vs Man U\nTottenham vs Man City\n\n Nice https://t.co/w71drEEvC9\
+        #NairaBetAt10 @TOTALNigeria 27th of June, 1967... If day and month counts \uD83D\uDE09 \
+        # RT @officialEFCC: If you feel like you aren't where you ought to be in life, make sure \
+        # to keep your hands doing something positive until de… @myzoto issue not resolved!! \
+        #  RT @FunnySayings: 5 rules of a relationship:\n\n1. stay faithful\n2. make them feel wanted\n \
+        # 3. respect your partner\n4. don't flirt with others… RT @TheFactsBook: 90% of the time it's not \
+        #  the person you miss, it's the feelings and moments you had when you were with them."
+
+clean_tweet(d_tweet)
--- solend_twitter_backup.py
+++ solend_twitter_backup.py
@@ -4,23 +4,31 @@ External Tool: Google Cloud Natural Language Processing Toolkit.
 Author: Adeyanju Victor (sorXCode)
 '''
 import re
+import os
 from zipfile import ZipFile
 import pandas as pd
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 
 class TweetsSentiment():
     '''
     TweetsSentiment Class: Extracts tweets from twitter_backup_file
     and perform sentiment analysis on tweet.
+
+    Return: int
     '''
 
     def __init__(self, zip_archive):
+        # def __init__(self):
         '''
         Initialize program with twitter_backup_file.zip
         '''
         self.zip_file = zip_archive
-        self.tweet_file, self.tweets, self.tweets_text = None, None, None
+        self.tweet_file, self.tweets, self.tweets_text_activity = None, None, None
         self.pattern = None
+        self.count = 0
 
     def extract_tweet_file(self):
         """
@@ -29,8 +37,10 @@ class TweetsSentiment():
         """
         try:
             self.tweet_file = ZipFile(self.zip_file).extract('tweet.js')
+            return self.tweet_file
         except FileNotFoundError:
             print('File not Found, Check path')
+            return 'Invalid path'
 
     def extract_tweets(self):
         '''
@@ -43,28 +53,83 @@ class TweetsSentiment():
             self.tweets = tmp.read()
 
         # Compile pattern matching tweet full_text in the tweet.js file
-        self.pattern = re.compile(r'\"full_text\"\s?:\s?(.+),\n')
+        self.pattern = re.compile(r'\"full_text\"\s?:\s?\"(.+)\",\n')
 
-        #'''
-        #Pandas Dataframe object created with label tweets to store list of
-        #tweets extracted via compiled regular expression pattern.
-        #'''
-        self.tweets_text = pd.DataFrame(data=self.pattern.findall(self.tweets),
-                                        columns=['tweets'], dtype='str')
-        return self.tweets_text
+        # '''
+        # Pandas Dataframe object created with label tweets to store list of
+        # tweets extracted via compiled regular expression pattern.
+        # '''
+        self.tweets = pd.Series(self.pattern.findall(self.tweets))
+        self.tweets = self.tweets.map(self.clean_tweets)
 
-    def analyze_tweets(self, tweet="Hello!"):
+        self.tweets_text_activity = pd.DataFrame(data=self.tweets,
+                                                 columns=['tweets'], dtype='str')
+
+        self.tweets_text_activity['sentiment'] = self.tweets_text_activity['tweets'].map(
+        self.analyze_tweets)
+        print(self.tweets_text_activity.head(20))
+        print(self.tweets_text_activity.describe())
+        self.tweets_text_activity.to_csv(
+            './data.csv', columns=self.tweets_text_activity.columns)
+        return self.tweets_text_activity
+
+    def clean_tweets(self, raw_tweet):
         '''
-        Analyzes tweets using Google's Natural Language Processing
-        (Sentiment Analysis) tool.
+        Removes special characters from tweets using regex and returns cleaned tweet
         '''
-        
-        return tweet.upper()
+        # unicode encoding does not support some characters used in tweet,
+        # encoding tweets as ascii for compartibility
+        raw_tweet = ascii(raw_tweet)
+        cleaned_tweet = ' '.join(re.sub(
+            r"(\w+:\/\/\S+)|(RT)|(\\u\S+)|(#)",
+            " ", raw_tweet).split())
+        cleaned_tweet = ' '.join(re.sub(
+            r"\\n+",
+            '. ', cleaned_tweet).split())
+        return cleaned_tweet
 
+    def analyze_tweets(self, text="Hello!"):
+        '''
+        Analyzes tweets using Google's AutoML Natural Language (Sentiment Analysis) tool.
+        '''
+        # Imports the Google Cloud client library
+        # from google.cloud import language
+        # from google.cloud.language import enums
+        # from google.cloud.language import types
+        # Instantiates a client
+        client = language.LanguageServiceClient()
+        # key = "AIzaSyDfD78IilMFoQW4TrtIlhcn5lKmA7W_Fmo"
+        # The text to analyze
+        # text = 'Every saint has a past. Every sinner has a future.'
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+
+        # Detects the sentiment of the text
+        sentiment = client.analyze_sentiment(
+            document=document).document_sentiment
+
+        print('Text: {}'.format(text))
+        print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+        print(f"{sentiment}")
+        return sentiment.score
+
+    def cleanup(self):
+        '''
+        Remove tmp files in path
+        '''
+        os.rmdir(self.tweet_file)
+
+    def main(self):
+        '''
+        Program Runner
+        '''
+        self.extract_tweet_file()
+        self.extract_tweets()
+        self.analyze_tweets()
+        self.cleanup()
 
 if __name__ == "__main__":
     FILE_ = 'temp.zip'
-    TWEETS = TweetsSentiment(FILE_)
-    TWEETS.extract_tweet_file()
-    TWEETS.extract_tweets()
-    print(TWEETS.tweets_text)
+    Tweetsentiment = TweetsSentiment(FILE_)
+    Tweetsentiment.main()
