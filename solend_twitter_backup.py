0dd760e1961bcef0f00301db50a8f11e7bb84776
sorXCode <sorXCode@gmail.com>
2019-06-26 17:31:46 +0100

    ADDEd Design & FlowChart

--- solend_twitter_backup.py
+++ solend_twitter_backup.py
@@ -16,12 +16,9 @@ class TweetsSentiment():
     '''
     TweetsSentiment Class: Extracts tweets from twitter_backup_file
     and perform sentiment analysis on tweet.
-
-    Return: int
     '''
 
     def __init__(self, zip_archive):
-        # def __init__(self):
         '''
         Initialize program with twitter_backup_file.zip
         '''
@@ -66,7 +63,7 @@ class TweetsSentiment():
                                                  columns=['tweets'], dtype='str')
 
         self.tweets_text_activity['sentiment'] = self.tweets_text_activity['tweets'].map(
-        self.analyze_tweets)
+            self.analyze_tweets)
         print(self.tweets_text_activity.head(20))
         print(self.tweets_text_activity.describe())
         self.tweets_text_activity.to_csv(
@@ -75,7 +72,9 @@ class TweetsSentiment():
 
     def clean_tweets(self, raw_tweet):
         '''
-        Removes special characters from tweets using regex and returns cleaned tweet
+        Removes special characters from tweets using regex 
+
+        return: string with reduced special characters
         '''
         # unicode encoding does not support some characters used in tweet,
         # encoding tweets as ascii for compartibility
@@ -91,21 +90,17 @@ class TweetsSentiment():
     def analyze_tweets(self, text="Hello!"):
         '''
         Analyzes tweets using Google's AutoML Natural Language (Sentiment Analysis) tool.
+
+        Return: Sentiment score (float)
         '''
-        # Imports the Google Cloud client library
-        # from google.cloud import language
-        # from google.cloud.language import enums
-        # from google.cloud.language import types
         # Instantiates a client
         client = language.LanguageServiceClient()
-        # key = "AIzaSyDfD78IilMFoQW4TrtIlhcn5lKmA7W_Fmo"
         # The text to analyze
-        # text = 'Every saint has a past. Every sinner has a future.'
         document = types.Document(
             content=text,
             type=enums.Document.Type.PLAIN_TEXT)
 
-        # Detects the sentiment of the text
+        # Calculates the sentiment of a text
         sentiment = client.analyze_sentiment(
             document=document).document_sentiment
 
@@ -128,6 +123,7 @@ class TweetsSentiment():
         self.extract_tweets()
         self.analyze_tweets()
         self.cleanup()
+
 
 if __name__ == "__main__":
     FILE_ = 'temp.zip'
